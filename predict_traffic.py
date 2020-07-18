7b74047c11a9a847d6f3571c5453373d423bb9bd
Gowthamram <40654487+KabirNarayan@users.noreply.github.com>
2020-07-18 15:18:19 +0530

    Add files via upload

---
+++ predict_traffic.py
@@ -0,0 +1,99 @@
+from PIL import Image
+import numpy as np
+import os
+
+def show_masked_img(roads_tensor, cars_tensor, raw_img, img_name):
+
+    roads_pred = roads_tensor.cpu().detach().numpy()
+    cars_pred = cars_tensor.cpu().detach().numpy()
+
+    roads_img_r = roads_pred[1]
+    roads_img_b = roads_pred[0]
+
+    cars_img_r = cars_pred[1]
+    cars_img_b = cars_pred[0]
+
+
+    roads_pred = roads_img_r > roads_img_b
+
+    cars_pred = cars_img_r > cars_img_b
+
+    cars_pred *= roads_pred
+
+    roads_pred *= np.logical_not(cars_pred)
+
+
+    img_array = np.array(raw_img).astype(np.uint8)
+
+    img_array //= 2
+
+    colored_array = np.zeros_like(img_array[:,:,0]) + 255
+
+
+    roads_mask = colored_array - img_array[:,:,0]
+    cars_mask = colored_array - img_array[:,:,2]
+    roads_mask *= roads_pred.astype(np.uint8)
+    cars_mask *= cars_pred.astype(np.uint8)
+
+
+    img_array[:,:,0] += roads_mask
+    img_array[:,:,2] += cars_mask
+    image = Image.fromarray(img_array, 'RGB')
+    image.save('dataset/traffic/' + img_name)
+    image.show()
+
+def get_traffic_level(roads_tensor, cars_tensor):
+
+    roads_pred = roads_tensor.cpu().detach().numpy()
+    cars_pred = cars_tensor.cpu().detach().numpy()
+
+    roads_img_r = roads_pred[1]
+    roads_img_b = roads_pred[0]
+
+    cars_img_r = cars_pred[1]
+    cars_img_b = cars_pred[0]
+
+
+    roads_pred = roads_img_r > roads_img_b
+
+    cars_pred = cars_img_r > cars_img_b
+
+    cars_pred *= roads_pred
+
+
+    road_pixels = np.sum(roads_pred)
+    total_pixels = roads_pred.shape[0] * roads_pred.shape[1]
+
+    if road_pixels / total_pixels < 0.1:
+        return "Not enough roads"
+    
+    #Check number of car pixels
+    car_pixels = np.sum(cars_pred)
+    traffic = car_pixels / road_pixels
+    # print("Traffic", traffic)
+    return traffic
+    # if traffic < 0.2:
+    #     return "No traffic"
+    # if traffic < 0.5:
+    #     return "Moderate traffic"
+    # if traffic < 0.8:
+    #     return "Traffic"
+    # return "Heavy traffic"
+
+if __name__ == "__main__":
+    import pickle
+    directory = 'dataset/car_preds'
+    num_of_files = len([name for name in os.listdir(directory) if os.path.isfile(os.path.join(directory, name))])
+    
+    for idx in range(1, num_of_files + 1):
+        str_idx = str(idx)
+
+        with open('dataset/car_preds/prediction_' + str_idx + '.pred', 'rb') as handle:
+            cars_pred, _ = pickle.load(handle)
+
+        with open('dataset/road_preds/prediction_' + str_idx + '.pred', 'rb') as handle:
+            roads_pred, raw_img = pickle.load(handle)
+        
+        img_name = ('0' * (7 - len(str_idx) + 1)) + str_idx + '.png'
+        show_masked_img(roads_pred, cars_pred, raw_img, img_name)
+        print(get_traffic_level(roads_pred, cars_pred))
